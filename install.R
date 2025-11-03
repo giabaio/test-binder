35ad60f43ef34d66d8b02cd1642d6cadf30401c3
Gianluca Baio <g.baio@ucl.ac.uk>
2025-11-03 14:43:34 +0000

    Test with deb that has no depenendecies installed prior

--- install.R
+++ install.R
@@ -1,1 +1,1 @@
-#install.packages(c("ggplot2","dplyr","BCEA","R2OpenBUGS","R2jags","manipulate","purrr","tidyr"))
+install.packages(c("ggplot2","dplyr","BCEA","R2OpenBUGS","R2jags","manipulate","purrr","tidyr"))
